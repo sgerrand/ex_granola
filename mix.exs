a96d440030bfd6ca9fa72014451bbfcd776386bc
Sasha Gerrand <sgerrand@users.noreply.github.com>
2026-04-19 17:04:41 +0100

    docs: generate HexDocs compatible documentation

--- mix.exs
+++ mix.exs
@@ -2,11 +2,12 @@ defmodule Granola.MixProject do
   use Mix.Project
 
   @repo_url "https://github.com/sgerrand/ex_granola"
+  @version "0.0.0"
 
   def project do
     [
       app: :granola,
-      version: "0.0.0",
+      version: @version,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -14,7 +15,11 @@ defmodule Granola.MixProject do
       # Hex
       package: package(),
       description: description(),
-      source_url: @repo_url
+      source_url: @repo_url,
+
+      # Docs
+      name: "Granola",
+      docs: docs()
     ]
   end
 
@@ -27,6 +32,7 @@ defmodule Granola.MixProject do
   defp deps do
     [
       {:req, "~> 0.5"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:plug, "~> 1.0", only: :test}
     ]
   end
@@ -35,13 +41,22 @@ defmodule Granola.MixProject do
     "Elixir client for the Granola API"
   end
 
+  defp docs do
+    [
+      extras: ["README.md", "CHANGELOG.md", "LICENSE"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @repo_url
+    ]
+  end
+
   defp package do
     [
       licenses: ["BSD-2-Clause"],
       links: %{
         "GitHub" => @repo_url,
-      },
-      name: "Granola"
+        "Changelog" => "https://hexdocs.pm/granola/changelog.html"
+      }
     ]
   end
 end
